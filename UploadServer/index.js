c1cbd55f61234f4f5033591ba24b2563c67614e9
Conor <conormcdonald6@gmail.com>
2020-02-07 19:30:26 +0000

    Node.js uploadserver for file uploads

---
+++ UploadServer/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const multer = require('multer');
+const cors = require('cors');
+const app = express();
+const upload = multer({
+dest:'./uploads/' //Upload to server folder
+})
+
+app.use(cors());
+
+app.post('/upload/',upload.single('file'), (req, res) => { // Look for post requests with /upload url , give to multer
+
+        try{
+          console.log(req.originalUrl);
+
+          res.json({file:req.file});  // Return the details of the uploaded file to the browser
+      }catch (err){
+        res.status(422).json({err});  // Send error 422(Unable to process)
+      }
+});
+
+app.get('/upload',function(req,res) {
+	res.send('(Upload Something)')
+});
+app.listen(3000,()=> console.log("Running on localhost:3000"));
