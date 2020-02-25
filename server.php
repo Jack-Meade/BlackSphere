92beac9c20852092552c51924af9bed361ea5248
danielsleo <117430494@umail.ucc.ie>
2020-02-25 17:44:22 +0000

    Created modal popup for uploading a file using bootstrap

--- server.php
+++ server.php
@@ -12,7 +12,7 @@
         <meta charset="UTF-8">
         <title>BlackSphere | <?php
                 if ($_SERVER['REQUEST_URI'] != "/bs/" && $_SERVER['REQUEST_URI'] != "/bs/?hidden")
-                    { $dir_path = str_replace("/bs", "", $_SERVER['REQUEST_URI']); }
+                    { $dir_path = str_replace("/test", "", $_SERVER['REQUEST_URI']); }
                 else
                     { $dir_path = "/bs/"; }
 
@@ -20,15 +20,20 @@
                 echo($dir_path);
             ?></title>
         <link rel="shortcut icon" href="/bs/images/favicon.png"/>
-        <link rel="stylesheet" href="/bs/style.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.7.0/dropzone.css"> <!--Temporary -->
-        <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"> -->
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+        <!-- End of bootstrap -->
+        <link rel="stylesheet" href="/bs/style.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+
         <script src="/bs/sorttable.js"></script>
         <script src="/bs/requests.js"></script>
-        <script src='/test/dropzone.js' type='text/javascript'></script>
-        <script src='/test/dzOptions.js' type='text/javascript'></script>
+        <script src='/bs/dropzone.js' type='text/javascript'></script>
+        <script src='/bs/dzOptions.js' type='text/javascript'></script>
     </head>
 
     <body>
@@ -56,16 +61,16 @@
                             if($_SERVER['QUERY_STRING']=="hidden") {
                                 $hide="";
                                 $ahref="./";
-                                $atext="Hide";
+                                $atext="<i class=\"fas fa-eye-slash\"></i> Hide";
                             } else {
                                 $hide=".";
                                 $ahref="./?hidden";
-                                $atext="Show";
+                                $atext="<i class=\"fas fa-eye\"></i> Show";
                             }
 
                             // Opens directory, trimming URL to map onto directory structure
                             if (strpos($_SERVER['REQUEST_URI'], "/bs/") !== false) {
-                                $dir_path = str_replace("/bs", "../bs", $_SERVER['REQUEST_URI']);
+                                $dir_path = str_replace("/test", "../test", $_SERVER['REQUEST_URI']);
                             }
 
 
@@ -150,8 +155,9 @@
 
             </form>
             <div id="butt_div">
-                <button form="dir_form" type="submit" class="fabutton"> <i class="fas fa-download"></i> Download</button>
-                <?php echo("<button type='button' class='fabutton' onclick= window.location.href='$ahref'><i class=\"fas fa-eye\"></i> $atext hidden files</button>"); ?>
+                <button form="dir_form" type="submit" class="btn btn-primary"> <i class="fas fa-download"></i> Download</button>
+                <?php echo("<button type='button' class=\"btn btn-primary\" onclick= window.location.href='$ahref'> $atext hidden files</button>"); ?>
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalUpload"> Upload </button>
             </div>
 
             <?php
@@ -167,13 +173,34 @@
                 }
             ?>
 
-            <button id="submit-all">Submit all files</button>
-            <div id="dropzone">
-            <form action="/upload" class="dropzone" id="myDropzone">
-              <p class="dz-message">Drop your files here, then click submit to  upload them</p>
-            </form>
-           </div>
 
+
+
+           <!-- Modal -->
+           <div class="modal fade" id="modalUpload" tabindex="-1" role="dialog" aria-labelledby="modalUpload" aria-hidden="true">
+             <div class="modal-dialog modal-dialog-centered" role="document">
+               <div class="modal-content">
+                 <div class="modal-header">
+                   <h5 class="modal-title" id="modalUploadTitle">Upload</h5>
+                   <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                       <span aria-hidden="true">&times;</span>
+                   </button>
+                 </div>
+                 <div class="modalUploadBody">
+                     <!-- <button id="submit-all">Submit all files</button> -->
+                     <div id="dropzone">
+                     <form action="/upload" class="dropzone" id="myDropzone">
+                       <p class="dz-message">Drop your files here, then click submit to  upload them</p>
+                     </form>
+                    </div>
+                 </div>
+                 <div class="modalUploadFooter">
+                   <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                   <button type="button" id="submit-all" class="btn btn-primary">Upload</button>
+                 </div>
+               </div>
+             </div>
+           </div>
 
             <form action="/bs/mounter.php" method="POST" id="mountRequestForm">
                 <h1>Mount your folder to BlackSphere</h1>
