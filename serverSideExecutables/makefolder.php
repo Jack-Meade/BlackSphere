c5c313ce6d5b0afd6fd6c03b1bcdcc12587a0944
alexsmith <alexsmith@live.ie>
2020-02-27 19:54:01 +0000

    changes

--- serverSideExecutables/makefolder.php
+++ serverSideExecutables/makefolder.php
@@ -3,7 +3,6 @@
     $new_file_name = $_POST['folderName'];
     chdir('..');
     $fullPath = $current_working_dir.$new_file_name;
-
     mkdir($fullPath); # This will make a folder in the currect directory that the script is in by default!
     #echo "$current_working_dir";
     echo "$fullPath";
