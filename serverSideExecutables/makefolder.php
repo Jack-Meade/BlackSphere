19b0e71a6bf8b22d9aa3f1f0404b32bd2ad2fbbf
alexsmith <alexsmith@live.ie>
2020-02-27 19:16:51 +0000

    more changes that i have to push to see if it works or not, Thanks Alex, WW

--- serverSideExecutables/makefolder.php
+++ serverSideExecutables/makefolder.php
@@ -3,6 +3,7 @@
     $baseDir = '/bs/TopLevelDir';
     $fullPath = $baseDir+'/'+$current_working_dir;
     mkdir($fullPath); # This will make a folder in the currect directory that the script is in by default!
+    echo "$current_working_dir";
     echo "$fullPath";
     echo "wewoo";
 ?>
