a2c59097ae668841bcd73331a14f0b0989f3674c
Jack-Meade <jack-meade@outlook.com>
2020-02-27 13:36:26 +0000

    Change login.php to read password from file

--- login.php
+++ login.php
@@ -1,36 +1,40 @@
 <?php
     session_start();
-    $key = 'asd';
+    $passwd = file_get_contents("passwd.txt");
+    $passwd = preg_replace('/\s/', '', $passwd);
 
     function gen_html() {
         echo('<!DOCTYPE html>
-        <html>
-            <head>
-                <meta charset="UTF-8">
-                <title>BlackSphere Login</title>
-                <link rel="shortcut icon" href="/bs/images/favicon.png"/>
-                <link rel="stylesheet" href="/bs/style.css"/>
-            </head>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>BlackSphere Login</title>
+        <link rel="shortcut icon" href="/bs/images/favicon.png"/>
+        <link rel="stylesheet" href="/bs/style.css"/>
+    </head>
 
-            <body>
-                <img src="/bs/blackspherelogo.png"/>
-                <form action="" method="POST" enctype=”multipart/form-data”>
-                    <label for="nkey">Login:</label>
-                    <input type="text" name="nkey" id="nkey" placeholder="Enter key here"/>
-                    <input type="submit" value="Login"/>
-                </form>
-            </body>
-        </html>');
+    <body>
+        <img src="/bs/blackspherelogo.png"/>
+        <form action="" method="POST" enctype=”multipart/form-data”>
+            <label for="nkey">Login:</label>
+            <input type="text" name="nkey" id="nkey" placeholder="Enter key here"/>
+            <input type="submit" value="Login"/>
+        </form>
+    </body>
+</html>');
     }
 
     if ($_SESSION['authenticated'] === true) {
         header('Location: /bs/TopLevelDir/');
     } elseif (!empty($_POST)) {
-        if ($_POST['nkey'] === $key) {
+        if ($_POST['nkey'] === $passwd) {
             $_SESSION['authenticated'] = true;
             header('Location: /bs/TopLevelDir/');
         } else {
             gen_html();
+            var_dump($_POST['nkey']);
+            echo "<br>";
+            var_dump($passwd);
         }
     } else {
         gen_html();
