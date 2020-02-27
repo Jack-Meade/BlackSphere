dd53efae83f764c094f1e50459f7fab668274313
alexsmith <alexsmith@live.ie>
2020-02-27 18:48:59 +0000

    Finishing the last touches on the makefulder

--- serverSideExecutables/makefolder.php
+++ serverSideExecutables/makefolder.php
@@ -1,4 +1,6 @@
 <?php
     $current_working_dir=$_POST['cwd'];
-    mkdir("testing"); # This will make a folder in the currect directory that the script is in by default!
+    $baseDir = 'TopLevelDir';
+    $fullPath = $baseDir+'/'+$current_working_dir;
+    mkdir($fullPath); # This will make a folder in the currect directory that the script is in by default!
 ?>
