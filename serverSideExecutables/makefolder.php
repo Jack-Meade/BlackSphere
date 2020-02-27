4cea73dc5f3e7224a7d5b7ac9eeca3dc41d7b8fc
alexsmith <alexsmith@live.ie>
2020-02-27 19:55:27 +0000

    weewoo

--- serverSideExecutables/makefolder.php
+++ serverSideExecutables/makefolder.php
@@ -2,7 +2,7 @@
     $current_working_dir=$_POST['currentFolder'];
     $new_file_name = $_POST['folderName'];
     chdir('..');
-    $fullPath = $current_working_dir.$new_file_name;
+    $fullPath = '.'.$current_working_dir.$new_file_name;
     mkdir($fullPath); # This will make a folder in the currect directory that the script is in by default!
     #echo "$current_working_dir";
     echo "$fullPath";
