f567dec32f030c98a01abd3b8e3a9e8cbeaf2946
alexsmith <alexsmith@live.ie>
2020-02-27 14:20:55 +0000

    expecting a json array to arriave with information like what mounted directory the user is currently in.

--- folderMaker/makefolder.php
+++ folderMaker/makefolder.php
@@ -1,3 +1,4 @@
 <?php
-    mkdir("testing") # This will make a folder in the currect directory that the script is in by default!
+    $the_good_stuff = json_decode($good_stuff);
+    mkdir("testing"); # This will make a folder in the currect directory that the script is in by default!
 ?>
