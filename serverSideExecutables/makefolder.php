192d791382d0fd6e0e59a9c5a889a48aa8a12ef0
alexsmith <alexsmith@live.ie>
2020-02-27 19:04:33 +0000

    adding some debug lines

--- serverSideExecutables/makefolder.php
+++ serverSideExecutables/makefolder.php
@@ -3,4 +3,5 @@
     $baseDir = '../TopLevelDir';
     $fullPath = $baseDir+'/'+$current_working_dir;
     mkdir($fullPath); # This will make a folder in the currect directory that the script is in by default!
+    echo "$fullPath"
 ?>
