0891d05ed700ec769a2a8b9abbaffc333eedc2be
danielsleo <117430494@umail.ucc.ie>
2020-03-02 00:00:38 +0000

    Beautify login page.

--- login.php
+++ login.php
@@ -9,17 +9,22 @@
     <head>
         <meta charset="UTF-8">
         <title>BlackSphere Login</title>
-        <link rel="shortcut icon" href="/bs/images/favicon.png"/>
-        <link rel="stylesheet" href="/bs/style.css"/>
+        <link rel="shortcut icon" href="/test/images/favicon.png"/>
+        <link rel="stylesheet" href="/test/style.css"/>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
     </head>
 
     <body>
+        <div id="blacklogo">
+            <img src="/test/blackspherelogo.png"/>
+        </div>
         <div id="login_form">
-            <img src="/bs/blackspherelogo.png"/>
-            <form action="" method="POST" enctype=”multipart/form-data”>
-                <label for="nkey">Login:</label>
-                <input type="text" name="nkey" id="nkey" placeholder="Enter key here"/>
-                <input type="submit" value="Login"/>
+            <form action="" method="POST" enctype=”multipart/form-data” class="form-inline">
+                <input type="text" class="form-control" name="nkey" id="nkey" placeholder="Enter key here"/>
+                <button type="submit" class="btn btn-secondary" ">Login</button>
             </form>
         </div>
     </body>
@@ -27,11 +32,11 @@
     }
 
     if ($_SESSION['authenticated'] === true) {
-        header('Location: /bs/TopLevelDir/');
+        header('Location: /test/TopLevelDir/');
     } elseif (!empty($_POST)) {
         if ($_POST['nkey'] === $passwd) {
             $_SESSION['authenticated'] = true;
-            header('Location: /bs/TopLevelDir/');
+            header('Location: /test/TopLevelDir/');
         } else {
             gen_html();
         }
