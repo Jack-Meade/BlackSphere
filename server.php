9abe6ef5c50aa139d8f50c0802491dd37ab1ce1b
Jack Meade <jack-meade@outlook.com>
2020-03-02 16:29:41 +0000

    Remove refresh button as it now automatic

--- server.php
+++ server.php
@@ -88,7 +88,6 @@
                 <button type="button" data-target="#modalUpload" class="btn btn-primary" data-toggle="modal" ><i class="fas fa-upload"></i> Upload </button>
                 <button type="button" data-target="#modalMount" class="btn btn-primary" data-toggle="modal" ><i class="fas fa-cloud-upload-alt"></i> Mount Folder </button>
                 <button type="button" data-target="#modalmkdir" class="btn btn-primary" data-toggle="modal"><i class="fas fa-folder"></i> Make Folder </button>
-                <button type="button" class="btn btn-primary" data-toggle="modal" onclick="refresh()"><i class="fas fa-sync"></i> Refresh </button>
                 <?php echo("<button type='button' class=\"btn btn-primary\" onclick= window.location.href='$ahref'> $atext hidden files</button>"); ?>
             </div>
 
