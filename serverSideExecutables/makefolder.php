1d57c44b19467a0b5e5c5ff434e94cb7f4d85a36
alexsmith <alexsmith@live.ie>
2020-03-02 10:18:50 +0000

    adding server document root into a variable

--- serverSideExecutables/makefolder.php
+++ serverSideExecutables/makefolder.php
@@ -1,7 +1,9 @@
 <?php
     $current_working_dir=$_POST['currentFolder'];
     $new_file_name = $_POST['folderName'];
-    chdir($_SERVER['DOCUMENT_ROOT']);
+    $var = $_SERVER['DOCUMENT_ROOT'];
+    echo $var;
+    chdir($var);
     #chdir('/home/sshfs/webdir/');
     $fullPath = '.'.$current_working_dir.$new_file_name;
     mkdir($fullPath); # This will make a folder in the currect directory that the script is in by default!
