4219b890ed37dbb55b1b5b37165a64c361279d09
AhernEYeldarB <ahernebradley@gmail.com>
2020-03-03 15:56:55 +0000

    Remove hard coded path drom $var

--- serverSideExecutables/makefolder.php
+++ serverSideExecutables/makefolder.php
@@ -2,8 +2,8 @@
     $current_working_dir=$_POST['currentFolder'];
     $new_file_name = $_POST['folderName'];
     $var = $_SERVER['DOCUMENT_ROOT'];
-    chdir($var.'/bs');
+    chdir($var);
     $fullPath = '.'.$current_working_dir.$new_file_name;
     mkdir($fullPath); # This will make a folder in the currect directory that the script is in by default!
-    #echo "$current_working_dir";
+    // echo "$current_working_dir $fullPath $new_file_name";
 ?>
