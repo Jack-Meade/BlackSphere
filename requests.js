3cf7db7a22b76e53e653d30883472208c30fe170
AhernEYeldarB <ahernebradley@gmail.com>
2020-03-03 15:56:23 +0000

    Ajax script for adding new folder

--- requests.js
+++ requests.js
@@ -39,3 +39,24 @@ function refresh() {
         }
     });
 }
+
+
+function makeFolder(name) {
+    $.ajax({
+        url: "/bs/serverSideExecutables/makefolder.php", // the endpoint
+        type: "POST", // http method
+        data: { currentFolder: window.location.href.substring(window.location.hostname.length + 8), folderName: name }, // data sent with the post request
+
+        // handle a successful response
+        success: function (html_body) {
+            // On success show something to user
+            refresh();
+        },
+
+        error: function (xhr, errmsg, err) {
+            // $('#results').html("<div class='alert-box alert radius' data-alert>Oops! We have encountered an error: " + errmsg +
+            //     " <a href='#' class='close'>&times;</a></div>"); // add the error to the dom
+            console.log(xhr.status + ": " + xhr.responseText); // provide a bit more info about the error to the console
+        }
+    });
+}
