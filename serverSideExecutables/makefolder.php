2e3885efa74f70e9955670724d4013ead8dfcb75
alexsmith <alexsmith@live.ie>
2020-03-03 15:19:55 +0000

    changed the hardcoded path to use server document root.

--- serverSideExecutables/makefolder.php
+++ serverSideExecutables/makefolder.php
@@ -1,10 +1,9 @@
 <?php
     $current_working_dir=$_POST['currentFolder'];
     $new_file_name = $_POST['folderName'];
-    chdir('/home/sshfs/webdir/');
+    $var = $_SERVER['DOCUMENT_ROOT'];
+    chdir($var.'/bs');
     $fullPath = '.'.$current_working_dir.$new_file_name;
     mkdir($fullPath); # This will make a folder in the currect directory that the script is in by default!
     #echo "$current_working_dir";
-    echo "$fullPath";
-    
 ?>
