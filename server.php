0842009ccf8ff0152d0e3d40e8d541452111f071
AhernEYeldarB <ahernebradley@gmail.com>
2020-03-04 18:27:21 +0000

    Add back in refrsh button for unspecified reasons

--- server.php
+++ server.php
@@ -88,6 +88,7 @@
                 <button type="button" data-target="#modalUpload" class="btn btn-primary" data-toggle="modal" ><i class="fas fa-upload"></i> Upload </button>
                 <button type="button" data-target="#modalMount" class="btn btn-primary" data-toggle="modal" ><i class="fas fa-cloud-upload-alt"></i> Mount Folder </button>
                 <button type="button" data-target="#modalmkdir" class="btn btn-primary" data-toggle="modal"><i class="fas fa-folder"></i> Make Folder </button>
+                <button type="button" class="btn btn-primary" data-toggle="modal" onclick="refresh()"><i class="fas fa-sync"></i> Refresh </button>
                 <?php echo("<button type='button' class=\"btn btn-primary\" onclick= window.location.href='$ahref'> $atext hidden files</button>"); ?>
             </div>
 
@@ -192,7 +193,7 @@
         </div>
         <script>
             mainVali();
-            setInterval(refresh, 5000); // Refresh the directory every 5 seconds
+            // setInterval(refresh, 5000); // Refresh the directory every 5 seconds
 
             $('#modalmkdirForm').on('submit', function (event) {
                 event.preventDefault();
